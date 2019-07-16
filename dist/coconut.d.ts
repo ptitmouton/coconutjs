1b70d66f9024f04bc1a36e7d888b0beebb15f290
Alexis Rinaldoni <arinaldoni@me.com>
2019-07-16 13:53:39 +0200

    add declaration file

---
+++ dist/coconut.d.ts
@@ -0,0 +1,58 @@
+export interface JobOutputFormat {
+    'mp4': string | undefined;
+    'mp4:hevc': string | undefined;
+    'webm': string | undefined;
+    'webm:vp9': string | undefined;
+    'avi': string | undefined;
+    'asf': string | undefined;
+    'mpegts': string | undefined;
+    'mov': string | undefined;
+    'flv': string | undefined;
+    'mkv': string | undefined;
+    '3gp': string | undefined;
+    'ogv': string | undefined;
+    'ogg': string | undefined;
+    'mp3': string | undefined;
+    'jpg': string | undefined;
+    'png': string | undefined;
+    'gif': string | undefined;
+    [key: string]: string | undefined;
+}
+export interface CreateJobOptions {
+    api_version?: string;
+    conf?: string;
+    vars?: {
+        [varname: string]: string;
+    };
+    api_key: string;
+    source: string;
+    webhook: string;
+    outputs: JobOutputFormat;
+}
+export interface Job {
+    id: number;
+    output_urls: string[];
+    event: string;
+    errors?: JobOutputFormat;
+    error_code?: string;
+    error_message?: string;
+}
+declare type Callback<T> = (error: (null | Error), job?: T) => void;
+export default class CoconutJS {
+    private apiKey;
+    constructor(apiKey?: string | undefined);
+    createJob(options: CreateJobOptions): Promise<Job>;
+    createJob(options: CreateJobOptions, callback: Callback<Job>): void;
+    getJob(jobId: string): Promise<Job>;
+    getJob(jobId: string, callback: Callback<Job>): void;
+    getAllMetadata(jobId: string): Promise<Job>;
+    getAllMetadata(jobId: string, callback: Callback<Job>): void;
+    getAllMetadataFor(jobId: string, sourceOrOutput: string): Promise<Job>;
+    getAllMetadataFor(jobId: string, sourceOrOutput: string, callback: Callback<Job>): void;
+    private sendCoconutRequest;
+    private submit;
+    private get;
+    private getRequestOptions;
+    private getConfig;
+}
+export {};
